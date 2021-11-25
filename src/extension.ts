34367fbf7af5c4817cda3fc90a2829f24a750dd2
Ygor Azambuja <ygorazambuja@gmail.com>
2021-11-24 22:49:38 -0400

    fix: remove comments and fix namespaced

--- src/extension.ts
+++ src/extension.ts
@@ -1,24 +1,10 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import path = require("path");
 import * as vscode from "vscode";
 
 const EXTENSION_NAME = "vuex-store-generator";
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
-  console.log(
-    'Congratulations, your extension "vuex-store-generator" is now active!'
-  );
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-
-  let generate = vscode.commands.registerCommand(
+  const generate = vscode.commands.registerCommand(
     `${EXTENSION_NAME}.generate`,
     handleCreateStore
   );
@@ -67,7 +53,7 @@ import getters from "./getters";
 import state from "./state";
 
 export default {
-  namespace: true,
+  namespaced: true,
   state,
   mutations,
   actions,
