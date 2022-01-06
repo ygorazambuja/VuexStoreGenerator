85c32d882cd29bbe473db618126249bd64a8160e
joaosilvabefore <92741701+joaosilvabefore@users.noreply.github.com>
2022-01-06 10:25:08 -0300

    Feature: Create command for generate module with mutation types (#3)
    * ♻️ refacotr: abstraction in generating template-based files
    
    * ✨ feat: Create command for generate mutation types

--- src/builders/index.ts
+++ src/builders/index.ts
@@ -2,13 +2,25 @@ import { window } from "vscode";
 
 import { createStoreDirectory } from "./createStoreDirectory";
 import { CreatorFileByTemplate } from "./CreatorFileByTemplate";
-import { TEMPLATES } from "./templates";
+import { TEMPLATES, TEMPLATES_MUATION_TYPES } from "./templates";
 
 export async function createStore(path: string, storeName: string) {
   createStoreDirectory(path, storeName);
 
   const STORE_PATH = `${path}/${storeName}`;
   const creatorFileByTemplate = new CreatorFileByTemplate(TEMPLATES, STORE_PATH);
+  creatorFileByTemplate.generateAllTemplateFiles();
+  
+  window.showInformationMessage(
+    `Store ${storeName} has been created in ${path}`
+  );
+}
+
+export async function createStoreWithMutationTypes(path: string, storeName: string) {
+  createStoreDirectory(path, storeName);
+
+  const STORE_PATH = `${path}/${storeName}`;
+  const creatorFileByTemplate = new CreatorFileByTemplate(TEMPLATES_MUATION_TYPES, STORE_PATH);
   creatorFileByTemplate.generateAllTemplateFiles();
   
   window.showInformationMessage(
--- src/builders/templates.ts
+++ src/builders/templates.ts
@@ -1,28 +1,28 @@
-export const TEMPLATES= [
+export const TEMPLATES = [
   {
-    name: 'state',
-    filename: 'state.js',
+    name: "state",
+    filename: "state.js",
     template: `export default {
       example: {}		
     }`,
   },
   {
-    name: 'actions',
-    filename: 'actions.js',
+    name: "actions",
+    filename: "actions.js",
     template: `export default {
       cleanExample: ({ commit }) => commit("updateExample", {})		
     }`,
   },
   {
-    name: 'mutations',
-    filename: 'mutations.js',
+    name: "mutations",
+    filename: "mutations.js",
     template: `export default {
       updateExample: (state, data) => state.example = data
     }`,
   },
   {
-    name: 'getters',
-    filename: 'getters.js',
+    name: "getters",
+    filename: "getters.js",
     template: `export default {
       payload(state, getters) {
         return state.example;
@@ -30,8 +30,8 @@ export const TEMPLATES= [
     }`,
   },
   {
-    name: 'index',
-    filename: 'index.js',
+    name: "index",
+    filename: "index.js",
     template: `import actions from "./actions";
     import mutations from "./mutations";
     import getters from "./getters";
@@ -44,5 +44,72 @@ export const TEMPLATES= [
       actions,
       getters,
     };`,
+  },
+];
+
+export const TEMPLATES_MUATION_TYPES = [
+  {
+    name: "state",
+    filename: "state.js",
+    template: `export default {
+  example: [],
+}
+`,
+  },
+  {
+    name: "actions",
+    filename: "actions.js",
+    template: `import mutationTypes from './mutation-types';
+export default {
+  async fetchExamples({ commit }) {
+    const data = await fetch('/examples');
+    commit(mutationTypes.SET_EXAMPLES, data);
+  },  	
+}
+    `,
+  },
+  {
+    name: "mutations",
+    filename: "mutations.js",
+    template: `import mutationTypes from './mutation-types';
+export default {
+  [mutationTypes.SET_EXAMPLES](state, data) {
+    state.examples = data;
+  },
+}
+`,
+  },
+  {
+    name: "mutations-types",
+    filename: "mutations-types.js",
+    template: `export default {
+  SET_EXAMPLES: 'SET_EXAMPLES'
+}`,
+  },
+  {
+    name: "getters",
+    filename: "getters.js",
+    template: `export default {
+  examplesChecked(state) {
+    return state.examples.filter(example => example.checked);
   }
+}
+`,
+  },
+  {
+    name: "index",
+    filename: "index.js",
+    template: `import actions from "./actions";
+import mutations from "./mutations";
+import getters from "./getters";
+import state from "./state";
+    
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters,
+};`,
+  },
 ];
