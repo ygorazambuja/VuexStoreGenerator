928f18a60d7d3e6351e32aaca62801a25d58f396
Ygor Azambuja <ygorazambuja@gmail.com>
2022-02-12 13:38:20 -0400

    feat: add createStoreTest

--- src/builders/index.ts
+++ src/builders/index.ts
@@ -1,29 +1,54 @@
 import { window } from "vscode";
 
-import { createStoreDirectory } from "./createStoreDirectory";
+import { createStoreDirectory, createTestDirectory } from "./utils/";
 import { CreatorFileByTemplate } from "./CreatorFileByTemplate";
-import { TEMPLATES, TEMPLATES_MUTATION_TYPES } from "./templates";
+import {
+  TEMPLATES,
+  TEMPLATES_MUTATION_TYPES,
+  TEMPLATES_TEST,
+} from "./templates";
 
 export async function createStore(path: string, storeName: string) {
   createStoreDirectory(path, storeName);
 
   const STORE_PATH = `${path}/${storeName}`;
-  const creatorFileByTemplate = new CreatorFileByTemplate(TEMPLATES, STORE_PATH);
+  const creatorFileByTemplate = new CreatorFileByTemplate(
+    TEMPLATES,
+    STORE_PATH
+  );
   creatorFileByTemplate.generateAllTemplateFiles();
-  
+
   window.showInformationMessage(
     `Store ${storeName} has been created in ${path}`
   );
 }
 
-export async function createStoreWithMutationTypes(path: string, storeName: string) {
+export async function createStoreWithMutationTypes(
+  path: string,
+  storeName: string
+) {
   createStoreDirectory(path, storeName);
 
   const STORE_PATH = `${path}/${storeName}`;
-  const creatorFileByTemplate = new CreatorFileByTemplate(TEMPLATES_MUTATION_TYPES, STORE_PATH);
+  const creatorFileByTemplate = new CreatorFileByTemplate(
+    TEMPLATES_MUTATION_TYPES,
+    STORE_PATH
+  );
   creatorFileByTemplate.generateAllTemplateFiles();
-  
+
   window.showInformationMessage(
     `Store ${storeName} has been created in ${path}`
   );
+}
+
+export async function createStoreTest(path: string) {
+  createTestDirectory(path);
+
+  const creatorFileByTemplate = new CreatorFileByTemplate(
+    TEMPLATES_TEST,
+    `${path}/__tests__`
+  );
+  creatorFileByTemplate.generateAllTemplateFiles();
+
+  window.showInformationMessage("Test directory has been created");
 }
